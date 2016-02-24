cd22636e02ce7558edcb2cb1a73f2c2d7ef7ca27
dawin-sistemas <dawin777@hotmail.com>
2016-02-24 15:50:24 -0500

    arreglo de home y template

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -1,32 +1,13 @@
-<?php 
+<?php
+
 header('Access-Control-Allow-Origin: *');
 defined('BASEPATH') OR exit('No direct script access allowed');
- 
+
 class Home extends MX_Controller {
-    
-<<<<<<< HEAD
-	function __construct() {
-        parent::__construct();
-        
-=======
-    public function index()
-    {
-        //$this->load->view('tpl_index');
+
+    public function index() {
         $data['module'] = 'home';
         $data['view_file'] = "tpl_hola";
-        echo Modules::run('template/admin',$data);
->>>>>>> 33163f00ca75f229034736c6f6b7460360c5c515
+        echo Modules::run('template/admin', $data);
     }
-
-
-    public function index(){
-        $data['module'] = 'home';
-        $data['view_file'] = 'home';
-        echo Modules::run('template/admin',$data);
-    }
-<<<<<<< HEAD
-
 }
-=======
-}
->>>>>>> 33163f00ca75f229034736c6f6b7460360c5c515
--- application/modules/template/controllers/Template.php
+++ application/modules/template/controllers/Template.php
@@ -1,31 +1,17 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
- 
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Template extends MX_Controller {
-<<<<<<< HEAD
-    
-   function admin($data){
-   		$this->load->view('template',$data);
-   }
 
-   function user(){
-   		//$this->load->view('admin');
-   }
-=======
- 	
-	function index(){
- 	
- 	}
+    function admin($data) {
+        $this->load->view('template', $data);
+    }
 
-	function one_col($data){
-		$this->load->view('one_col',$data);
-	}
+    function user() {
+        //$this->load->view('admin');
+    }
 
-	function two_col($data){
-		$this->load->view('two_col',$data);
-	}
+   
 
-	function admin($data){
-		$this->load->view('admin',$data);
-	}
->>>>>>> 33163f00ca75f229034736c6f6b7460360c5c515
 }
