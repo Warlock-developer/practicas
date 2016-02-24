728ec79a6acf2698e89485f38c51cce0a9b0cfa9
dawin-sistemas <dawin777@hotmail.com>
2016-02-24 16:24:04 -0500

    nuevo semcha

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -7,7 +7,7 @@ class Home extends MX_Controller {
 
     public function index() {
         $data['module'] = 'home';
-        $data['view_file'] = "tpl_hola";
+        $data['view_file'] = "home";
         echo Modules::run('template/admin', $data);
     }
 }
